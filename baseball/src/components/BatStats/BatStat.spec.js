de58d139a14ee2a6b30a7ab3b8ce255bbdf8f1bf
ajb85 <ajbrush85@gmail.com>
2019-04-09 17:00:30 -0400

    Verified strikes and balls are in the document...even though getBy kind of does that already

--- baseball/src/components/BatStats/BatStat.spec.js
+++ baseball/src/components/BatStats/BatStat.spec.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { render, cleanup } from "react-testing-library";
 
+import "jest-dom/extend-expect";
+
 afterEach(cleanup);
 
 import BatStats from "./BatStats.js";
 
 describe("<BatStats/>", () => {
-  it("Should have balls and strikes", () => {
+  it("Should have balls and strikes as a number", () => {
     const { getByRole } = render(<BatStats />);
-    getByRole("strikes");
-    getByRole("balls");
+    const strikes = getByRole("strikes");
+    const balls = getByRole("balls");
+
+    expect(strikes).toBeInTheDocument();
+    expect(balls).toBeInTheDocument();
   });
 });
